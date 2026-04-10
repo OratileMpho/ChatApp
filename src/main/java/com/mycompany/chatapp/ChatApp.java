2e352fadf3c34ea084f815a08bb1c7648199b5f5
OratileMpho <ST10535160@myemeris.edu.za>
2026-04-10 19:05:13 +0200

    this commit has been updated by allowing the user to enter their personal information

--- src/main/java/com/mycompany/chatapp/ChatApp.java
+++ src/main/java/com/mycompany/chatapp/ChatApp.java
@@ -38,63 +38,7 @@ public class ChatApp {
         }
 
     }
-    // creating a scanner that will allow user to enter password and this will aslo validate the password
-
-    public static void Password() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter password: ");
-        String Password = scanner.nextLine();
-
-        boolean containsCapital = false;
-        boolean hasANumber = false;
-        boolean SpecialCha = false;
-
-        for (char c : Password.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                containsCapital = true;
-            }
-            if (Character.isDigit(c)) {
-                hasANumber = true;
-            }
-            if (!Character.isLetterOrDigit(c)) {
-                SpecialCha = true;
-            }
-        }
-
-        boolean isValid = Password.length() >= 8 && containsCapital && hasANumber && SpecialCha;
-
-        if (isValid) {
-            System.out.println("Password was successfully captured");
-        } else {
-            System.out.println("Password does not meet requirements");
-        }
-
-        scanner.close();
-    }
-
-    //allowing the user to enter their cellphone number and also validating it 
-    public void CellPhone(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        String Cellphone = " ";
-
-        while (true) {
-
-            System.out.println("Please enter your cellphone number , make sure it includes the national code : "); // this willl ensure that the user enters their cellphone number that has the correct or relevent national code
-
-            Cellphone = scanner.nextLine().trim();
-
-            if (Cellphone.matches("\\+\\d{1,3}\\d{9}")) {// this a simple regex for the number to include the +27 code which identifies the nationality and also to make sure it has 9 digits
-
-                System.out.println("Cell phone number entered is valid");
-
-            } else {
-
-                System.out.println("Cellphone number is not recongnised, please ensure that the entered cellphone number has 9 digits with its correct valid national code");
-            }
-        }
-    }
-
+    
 }
     
     
