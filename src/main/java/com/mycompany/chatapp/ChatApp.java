a545e2669ed4104cbd33bb85ae33b48c4b272fa6
OratileMpho <ST10535160@myemeris.edu.za>
2026-04-10 19:02:51 +0200

    this commit has been updated by allowing the user to enter their personal information

--- src/main/java/com/mycompany/chatapp/ChatApp.java
+++ src/main/java/com/mycompany/chatapp/ChatApp.java
@@ -12,9 +12,21 @@ import java.util.Scanner;
  */
 public class ChatApp {
 
+    public void NameandSurname(String[] args){
+    
+       Scanner scanner = new Scanner(System.in);
+       
+       System.out.println("Please enter your name : ");
+       String firstname = scanner.nextLine();
+       
+       System.out.println("Please enter your last name : ");
+       String Lastname = scanner.nextLine();
+    
+    
+    }
+          // validating username   
     public void validateUsername(String[] args) {
-
-        //validating username 
+ 
         String Username = "";
         boolean isValid = Username.contains("_") && Username.length() <= 5;
         if (isValid) {
